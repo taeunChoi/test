f6504e9de6097bd9ba2ff0e9903f4fb4af26cc71
taeun <techoi0829@gmail.com>
2019-10-01 20:29:22 +0900

    Update 739.java

--- 739.java
+++ 739.java
@@ -11,4 +11,16 @@ class Solution {
         }
         return res;
     }
+    
+    
+//     public int[] dailyTemperatures(int[] T) {
+//         int[] ans = new int[T.length];
+//         Stack<Integer> stack = new Stack();
+//         for (int i = T.length - 1; i >= 0; --i) {
+//             while (!stack.isEmpty() && T[i] >= T[stack.peek()]) stack.pop();
+//             ans[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+//             stack.push(i);
+//         }
+//         return ans;
+//     }
 }
