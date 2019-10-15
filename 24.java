a57792520fa9c05713ad150cc6c89e9139e57cdb
taeun <techoi0829@gmail.com>
2019-10-15 20:52:24 +0900

    Create 24.java

---
+++ 24.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null) return null;
+        if(head.next == null) return head;
+        ListNode resHead = head.next;
+        
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        while(p2 != null) {
+            p1.next = p2.next == null? null :(p2.next.next==null? p2.next: (p2.next.next));
+            //swap
+            ListNode tmp = p2.next;
+            p2.next = p1;
+            p1 = tmp;
+            p2 = tmp == null? null: tmp.next;   
+        }
+        
+        
+        return resHead;
+    }
+}
